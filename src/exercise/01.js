93da035d64f04c59a13423338a0dc5d9406f6cff
José Gonçalves <goncalves2541@gmail.com>
2021-11-01 21:04:07 +0000

    challenge solved

---
+++ src/exercise/01.js
@@ -0,0 +1,17 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+function Counter({initialCount = 0, step = 1}) {
+  // 🐨 replace React.useState with React.useReducer.
+  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => setCount(count + step)
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
